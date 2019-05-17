c0b3d1db2e59c9a5a81ea0fb820597dd9accfe6c
marcelo.flores <marcelo.floresman@gmail.com>
2019-05-17 01:29:01 +0100

    first commit

---
+++ src/test/java/com/workday/words/AppTest.java
@@ -0,0 +1,20 @@
+package com.workday.words;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
